712278ae011f6fc2d3688f3edceb1eeec3aa7422
lucas <santanafx@hotmail.com>
2025-01-05 21:15:10 -0300

    fix: .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 import { HeroesModule } from './heroes/heroes.module';
 
+dotenv.config();
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGODB_URI), HeroesModule],
   controllers: [],
