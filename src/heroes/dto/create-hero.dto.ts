1d1d83a1adbcc0f016e9df26e069395e1d6c56ee
lucas <santanafx@hotmail.com>
2025-01-06 16:24:22 -0300

    feat: added more description to swagger documentation

--- src/heroes/dto/create-hero.dto.ts
+++ src/heroes/dto/create-hero.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateHeroDto {
+  @ApiProperty({
+    description: 'The name of the hero',
+    example: 'Sipderman',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'The abilities of the hero',
+    example: 'Sipder web',
+  })
   abilities: string;
+
+  @ApiProperty({
+    description: 'The origin of the hero',
+    example: 'New York',
+  })
   origin: string;
+
+  @ApiProperty({
+    description: 'The image of the hero',
+    example: 'Image path',
+  })
   image: string;
 }
