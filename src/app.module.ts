14209bdbdddc6a51ef2ee4cceddd3359b754de29
lucas <santanafx@hotmail.com>
2025-01-05 21:01:40 -0300

    refactor: env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HeroesModule } from './heroes/heroes.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://santanafx:!Lucas421@cluster0.epg7w.mongodb.net/',
-    ),
-    HeroesModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), HeroesModule],
   controllers: [],
   providers: [],
 })
